ca1b7070f6b65bfdc9efbdae38e4350b9e7a0e7b
Kayoum <86859599+Kymhd@users.noreply.github.com>
2022-04-07 17:44:19 +0000

    Create Find The Parity Outlier.py

---
+++ Codewars/Find The Parity Outlier.py
@@ -0,0 +1,31 @@
+"""
+You are given an array (which will have a length of at least 3, but could be very large) containing integers.
+The array is either entirely comprised of odd integers or entirely comprised 
+of even integers except for a single integer N. 
+Write a method that takes the array as an argument and returns this "outlier" N.
+
+Examples
+[2, 4, 0, 100, 4, 11, 2602, 36]
+Should return: 11 (the only odd number)
+
+[160, 3, 1719, 19, 11, 13, -21]
+Should return: 160 (the only even number)
+"""
+
+
+def find_outlier(list_integers):
+    odd = []
+    even = []
+    for num in list_integers:
+       if num % 2 != 0:
+         odd.append(num)
+       elif num % 2 == 0:
+         even.append(num)
+    if len(odd) < len(even):
+      return odd[0]
+    else:
+      return even[0]
+
+print((find_outlier([2, 4, 0, 100, 4, 11, 2602, 36]))) # ==> 3
+print((find_outlier([2, 4, 6, 8, 10, 3]))) # ==> 11
+print((find_outlier([160, 3, 1719, 19, 11, 13, -21]))) # ==> 160
