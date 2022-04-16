7c8c102d3427df280edee88bac1fd2199ec94b10
Kayoum <86859599+Kymhd@users.noreply.github.com>
2022-04-16 18:26:44 +0000

    Create count_sheep.py

---
+++ Codewars/Fundamentals/count_sheep.py
@@ -0,0 +1,10 @@
+def count_sheep(n):
+    # your code
+    if n == 0 :
+        return ""
+    if n == 1:
+        return "1 sheep..."
+    elif n == 2:
+        return "1 sheep...2 sheep..."
+    else:
+        return "1 sheep...2 sheep...3 sheep..."
