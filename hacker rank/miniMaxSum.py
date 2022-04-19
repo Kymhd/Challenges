baca866c92e31d8138ebd4c9918f7b2407b00d20
Kayoum <86859599+Kymhd@users.noreply.github.com>
2022-04-19 15:27:21 +0000

    Create miniMaxSum.py

---
+++ hacker rank/miniMaxSum.py
@@ -0,0 +1,30 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    # Write your code here
+    arr = sorted(arr)
+    minimum = 0
+    maximum = 0
+    for i in arr[0:-1]:
+        minimum += i
+    for i in arr[1:]:
+        maximum += i
+    print(minimum, maximum)
+    #print(sum(arr[:-1]),sum(arr[1:]))
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
