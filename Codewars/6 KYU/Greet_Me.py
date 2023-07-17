426ce949eaa28cc535948471d2877e4306461aa8
Kayoum <86859599+Kymhd@users.noreply.github.com>
2023-07-17 19:13:28 +0000

    Create Greet_Me.py

---
+++ Codewars/6 KYU/Greet_Me.py
@@ -0,0 +1,15 @@
+""" Write a method that takes one argument as name and then greets that name, 
+capitalized and ends with an exclamation point.
+
+Example:
+
+"riley" --> "Hello Riley!"
+"JACK"  --> "Hello Jack!"
+"""
+def greet(name): 
+    # your code here
+    return "Hello " + name.capitalize() + "!"
+
+(greet('riley'), 'Hello Riley!')
+(greet('molly'), "Hello Molly!")
+(greet('BILLY'), "Hello Billy!")
